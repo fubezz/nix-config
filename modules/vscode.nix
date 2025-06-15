a893d61f0edf17fbb5a4d8c3520b8538102f64a3
Fabin Spiess <fabian@aignostics.com>
2025-06-15 15:00:54 +0200

    Fix VS Code profiles configuration warnings
    - Migrated VS Code configuration to use the new profiles.default syntax
    - Moved keybindings, userSettings, and extensions under profiles.default
    - Eliminated all Home Manager warnings about deprecated VS Code options
    - All 17 extensions continue to work perfectly
    - Configuration now follows the latest Home Manager VS Code standards

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -4,76 +4,78 @@
   programs.vscode = {
     enable = true;
 
-    keybindings = [
-      # See https://code.visualstudio.com/docs/getstarted/keybindings#_advanced-customization
-      {
-          key = "shift+cmd+j";
-          command = "workbench.action.focusActiveEditorGroup";
-          when = "terminalFocus";
-        }
-    ];
-
-    userSettings = {
-        # This property will be used to generate settings.json:
-        # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
-        "editor.formatOnSave" = true;
-        "editor.codeActionsOnSave" = {
-          "source.organizeImports" = "explicit";
-        };
-        "editor.rulers" = [ 80 120 ];
-        "editor.minimap.enabled" = false;
-        "editor.bracketPairColorization.enabled" = true;
-        "editor.guides.bracketPairs" = "active";
-        "files.trimTrailingWhitespace" = true;
-        "files.insertFinalNewline" = true;
-        "files.trimFinalNewlines" = true;
-        "workbench.editor.enablePreview" = false;
-        "explorer.confirmDelete" = false;
-        "git.autofetch" = true;
-        "git.confirmSync" = false;
-        "terminal.integrated.fontSize" = 12;
-        "terminal.integrated.fontFamily" = "MesloLGS NF";
-
-        # Nix-specific settings
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nil";
-        "[nix]" = {
-          "editor.defaultFormatter" = "jnoortheen.nix-ide";
-          "editor.tabSize" = 2;
-        };
-
-        # File associations
-        "files.associations" = {
-          "*.nix" = "nix";
-          "flake.lock" = "json";
-        };
-    };
-
-    extensions = with pkgs.vscode-marketplace; [
-        # Nix development
-        jnoortheen.nix-ide
-        bbenoist.nix                        # Additional Nix language support
-        arrterian.nix-env-selector          # Nix environment selector
-        pinage404.nix-extension-pack        # Nix extension pack
-
-        # Theme
-        dracula-theme.theme-dracula
-
-        # Language support
-        redhat.vscode-yaml
-        hashicorp.terraform
-        ms-python.python
-        ms-python.black-formatter
-        ms-python.isort
-        ms-python.flake8                    # Python linting
-        tamasfe.even-better-toml
-
-        # DevOps and cloud tools
-        ms-kubernetes-tools.vscode-kubernetes-tools
-
-        # Git integration
-        github.vscode-pull-request-github
-        eamodio.gitlens
+    profiles.default = {
+      keybindings = [
+        # See https://code.visualstudio.com/docs/getstarted/keybindings#_advanced-customization
+        {
+            key = "shift+cmd+j";
+            command = "workbench.action.focusActiveEditorGroup";
+            when = "terminalFocus";
+          }
       ];
+
+      userSettings = {
+          # This property will be used to generate settings.json:
+          # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
+          "editor.formatOnSave" = true;
+          "editor.codeActionsOnSave" = {
+            "source.organizeImports" = "explicit";
+          };
+          "editor.rulers" = [ 80 120 ];
+          "editor.minimap.enabled" = false;
+          "editor.bracketPairColorization.enabled" = true;
+          "editor.guides.bracketPairs" = "active";
+          "files.trimTrailingWhitespace" = true;
+          "files.insertFinalNewline" = true;
+          "files.trimFinalNewlines" = true;
+          "workbench.editor.enablePreview" = false;
+          "explorer.confirmDelete" = false;
+          "git.autofetch" = true;
+          "git.confirmSync" = false;
+          "terminal.integrated.fontSize" = 12;
+          "terminal.integrated.fontFamily" = "MesloLGS NF";
+
+          # Nix-specific settings
+          "nix.enableLanguageServer" = true;
+          "nix.serverPath" = "nil";
+          "[nix]" = {
+            "editor.defaultFormatter" = "jnoortheen.nix-ide";
+            "editor.tabSize" = 2;
+          };
+
+          # File associations
+          "files.associations" = {
+            "*.nix" = "nix";
+            "flake.lock" = "json";
+          };
+      };
+
+      extensions = with pkgs.vscode-marketplace; [
+          # Nix development
+          jnoortheen.nix-ide                  # Main Nix language support
+          bbenoist.nix                        # Additional Nix language support
+          arrterian.nix-env-selector          # Nix environment selector
+          pinage404.nix-extension-pack        # Nix extension pack
+
+          # Theme
+          dracula-theme.theme-dracula
+
+          # Language support
+          redhat.vscode-yaml
+          hashicorp.terraform
+          ms-python.python
+          ms-python.black-formatter
+          ms-python.isort
+          ms-python.flake8                    # Python linting
+          tamasfe.even-better-toml
+
+          # DevOps and cloud tools
+          ms-kubernetes-tools.vscode-kubernetes-tools
+
+          # Git integration
+          github.vscode-pull-request-github
+          eamodio.gitlens
+        ];
+    };
   };
 }
